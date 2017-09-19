034c00fba2791b7efc6acbff26182e260fb04b45
Alfred Moore <ak_moore@live.com>
2017-09-19 00:16:23 -0500

    add post users route and token method

--- server/server.js
+++ server/server.js
@@ -91,6 +91,21 @@ app.patch('/todos/:id', (req, res) => {
     }).catch(error => {res.status(400).send();});
 });
 
+//POST /users
+
+app.post('/users', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+    var user = new User(body);
+
+    user.save().then(() => {
+        return user.generateAuthToken();
+    })
+    .then(token => {
+        res.header('x-auth', token).send(user);
+    })
+    .catch(err => res.status(400).send({message: 'Unable to create user.'}));
+});
+
 app.listen(PORT, () => {
     console.log(`Started on port ${PORT}`);
 })
